40c105b58ab56d2be3681e89a8efed6eda7f21dd
Pradeep Reddy Raamana <raamana@gmail.com>
2020-05-09 23:39:17 -0400

    version mgmt

--- ipypm/__init__.py
+++ ipypm/__init__.py
@@ -6,3 +6,7 @@ if version_info.major >= 3:
                        compare_tab, edit_tab, main_tab, edit_connectors_tab)
 else:
     raise NotImplementedError('pypm requires Python 3 or higher. Please upgrade!')
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
